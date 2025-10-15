8805c9bd4311a71cd6082112407e6b62278d8f52
sebastiansokolowski <ssokolowskisebastian@gmail.com>
2025-10-15 22:09:11 +0200

    Visit counter

--- taxi/views.py
+++ taxi/views.py
@@ -1,20 +1,25 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 from django.views import generic
 
 from .models import Driver, Car, Manufacturer
 
-
+@login_required
 def index(request):
     """View function for the home page of the site."""
 
     num_drivers = Driver.objects.count()
     num_cars = Car.objects.count()
     num_manufacturers = Manufacturer.objects.count()
+    num_visits = request.session.get('num_visits', 0)
+    num_visits += 1
+    request.session['num_visits'] = num_visits
 
     context = {
         "num_drivers": num_drivers,
         "num_cars": num_cars,
         "num_manufacturers": num_manufacturers,
+        "num_visits" : num_visits
     }
 
     return render(request, "taxi/index.html", context=context)
