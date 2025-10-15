ef531d20a65283380bf9a3b7279a242c2867511d
sebastiansokolowski <ssokolowskisebastian@gmail.com>
2025-10-15 22:56:19 +0200

    solution

--- taxi_service/settings.py
+++ taxi_service/settings.py
@@ -133,3 +133,6 @@ STATIC_ROOT = BASE_DIR / "staticfiles"
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+LOGIN_URL = "/login/"
+LOGIN_REDIRECT_URL = "/"
