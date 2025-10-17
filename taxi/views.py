062bbff16ce679063ab40d49c853bcf061b1d273
sebastiansokolowski <ssokolowskisebastian@gmail.com>
2025-10-17 13:26:12 +0200

    solution

--- taxi/views.py
+++ taxi/views.py
@@ -50,7 +50,7 @@ class DriverListView(LoginRequiredMixin, generic.ListView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['current_driver_id'] = getattr(self.request.user, 'id', None)
+        context["current_driver_id"] = getattr(self.request.user, "id", None)
         return context
 
 
